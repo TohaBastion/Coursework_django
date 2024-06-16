0c31f7ef636fcf8c3153688f8aab30ca4c17b0c4
Anton Nechay <a.a.nechay@gmail.com>
2024-06-16 18:20:33 +0300

    commit 16.06

--- main/models.py
+++ main/models.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
 from django.db import models
+import re
+
 
 class CustomUser(AbstractUser):
     phone_number = models.CharField(max_length=15, blank=True, null=True)
+    email = models.EmailField(unique=True)
+
